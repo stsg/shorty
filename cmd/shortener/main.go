39353ca40bf044a801e521cba0ece785b1d9bfbc
Stanislav Gobunov <stas@komus.ru>
2023-12-21 22:32:55 +0300

    adding logger

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,9 +12,12 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
 
 	"github.com/stsg/shorty/internal/config"
 	"github.com/stsg/shorty/internal/handle"
+	mylogger "github.com/stsg/shorty/internal/logger"
 	"github.com/stsg/shorty/internal/storage"
 )
 
@@ -24,12 +27,23 @@ func main() {
 	strg := storage.NewMapStorage()
 	hndl := handle.NewHandle(conf, *strg)
 
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(errors.New("cannot create logger"))
+	}
+	defer logger.Sync()
+
 	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(mylogger.ZapLogger(logger))
+	r.Use(middleware.Recoverer)
 
 	r.Post("/", hndl.HandleShortRequest)
 	r.Get("/{id}", hndl.HandleShortID)
 
-	err := http.ListenAndServe(conf.GetRunAddr(), r)
+	err = http.ListenAndServe(conf.GetRunAddr(), r)
 	if err != nil {
 		panic(errors.New("cannot run server"))
 	}
---
+++ internal/logger/logger.go
@@ -0,0 +1,67 @@
+package logger
+
+import (
+	"net/http"
+	"time"
+
+	// "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var Sugar zap.SugaredLogger
+
+func ZapLogger(logger *zap.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			var fields []zapcore.Field
+			var headers []string
+			for k := range r.Header {
+				headers = append(headers, k)
+			}
+
+			for _, h := range headers {
+				fields = append(fields, zap.String(h, r.Header.Get(h)))
+			}
+			logger.Info("header", fields...)
+
+			fields = []zap.Field{
+				zap.String("Method", r.Method),
+				zap.String("Host", r.Host),
+				zap.String("RequestURI", r.RequestURI),
+				zap.String("Proto", r.Proto),
+				zap.String("RemoteAddr", r.RemoteAddr),
+				zap.String("UserAgent", r.UserAgent()),
+				zap.Int64("ContentLength", r.ContentLength),
+			}
+			logger.Info(
+				"http request",
+				fields...)
+
+			then := time.Now()
+
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+			next.ServeHTTP(ww, r)
+
+			dur := time.Since(then)
+			status := ww.Status()
+			var responseFields []zapcore.Field = []zapcore.Field{
+				zap.Int("Status", status),
+				zap.Int("Bytes", ww.BytesWritten()),
+				zap.Duration("Duration", dur),
+			}
+			if status < 200 || (status > 399 && status < 400) || status > 499 {
+				logger.Error(
+					"http response",
+					responseFields...)
+			} else {
+				logger.Info(
+					"http response",
+					responseFields...)
+			}
+		})
+	}
+}
